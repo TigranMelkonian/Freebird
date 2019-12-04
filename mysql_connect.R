3b5120042f67034a0893e1debae1ff9fced77488
TigranMelkonian <tigmelkonian@gmail.com>
2019-12-04 15:47:45 -0500

    can connect to local DB now

--- mysql_connect.R
+++ mysql_connect.R
@@ -4,7 +4,7 @@ library(RMySQL)
 mysql_credentials <- read.csv(paste0(CREDENTIALS_PATH, '/mysql_credentials.csv'), stringsAsFactors = F)
 
 #Connect to redshift DB
-conn <- dbConnect(MySQL(), user = mysql_credentials$user, password = mysql_credentials$password, host = mysql_credentials$host, port = 3306, dbname = mysql_credentials$dbname)
+conn <- dbConnect(MySQL(), user = mysql_credentials$user, password = mysql_credentials$password, host = mysql_credentials$host, port = mysql_credentials$port, dbname = mysql_credentials$dbname)
 rs <- dbGetQuery(conn, 'set character set utf8')
 
 #Checking the schema you are connected to
